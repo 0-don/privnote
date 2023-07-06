6e3c65d5f37198d47ae77c1a56d6c038a1c93274
Don-Cryptus <don.cryptus@gmail.com>
2023-07-07 00:36:59 +0200

    test

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,7 +6,7 @@
 <header class="absolute left-0 right-0 top-0 bg-rose-600 py-1">
   <section class="container mx-auto flex items-center space-x-2 px-2">
     <img alt="logo" src="/privnote-logo.svg" />
-    <p class="w-64 text-sm">Send notes will self-destruct after being reayd.</p>
+    <p class="w-64 text-sm">Send notes will self-destruct after being read.</p>
   </section>
 </header>
 <slot />
