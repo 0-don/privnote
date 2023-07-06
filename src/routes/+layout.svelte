923ff6cef0dfc3d262193708aafe585410bab2e5
Don-Cryptus <don.cryptus@gmail.com>
2023-07-07 00:17:25 +0200

    test a

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,5 +4,10 @@
 
 </script>
 
-<header class="bg-red-600 i-mdi:abjad-arabic"><img alt="logo" src="/privnote-logo.svg" ></header>
+<main>
+<header class="bg-rose-600 flex justify-center items-center py-1 space-x-2">
+	<img alt="logo" src="/privnote-logo.svg" >
+	<p class="w-64 text-sm">Send notes will self-destruct after being read.</p>
+</header>
 <slot />
+</main>
