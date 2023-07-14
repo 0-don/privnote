e7dc7c324d5497ab821b68c104107270dd2acbf2
Don-Cryptus <don.cryptus@gmail.com>
2023-07-14 22:18:39 +0200

    clean up

--- web/src/lib/components/pages/debug/Debug.svelte
+++ web/src/lib/components/pages/debug/Debug.svelte
@@ -8,7 +8,7 @@
 </script>
 
 {#if dev}
-  <div class="absolute left-[5%] top-[5%] w-2/12 rounded-md bg-slate-900 px-5 py-4 shadow-xl">
+  <div class="absolute left-[5%] top-[5%] z-50 w-2/12 rounded-md bg-slate-900 px-5 py-4 shadow-xl">
     <h1 class="mb-2 mt-1 text-lg font-semibold leading-3 text-green-600">Debug</h1>
     {#each entries as [title, messages]}
       <div class="flex flex-col space-y-0.5">
