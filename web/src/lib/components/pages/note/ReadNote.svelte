d806d34be935c5eb9123fd0da6027e68940f25d4
Don-Cryptus <don.cryptus@gmail.com>
2023-07-16 20:57:12 +0200

    test

--- web/src/lib/components/pages/note/ReadNote.svelte
+++ web/src/lib/components/pages/note/ReadNote.svelte
@@ -4,20 +4,30 @@
 
   export let data: ResponseBody<NoteResponse>;
   export let title = 'Note contents';
+
+  const error = data?.messages?.find(({ path }) => path === 'error')?.message || '';
 </script>
 
 <div class="mt-3 flex items-center justify-start">
   <h1 class="my-2 text-2xl font-bold">{title}</h1>
 </div>
-<div class="bg-alert text-center p-1 mb-3">{data.data?.alert}</div>
 
-<section id="content">
-  <textarea
-    rows="13"
-    name="note"
-    value={data.data?.note.note}
-    readonly
-    placeholder="Write your note here..."
-    class="w-full !bg-yellow-100 !bg-opacity-75 p-5 text-black outline-none placeholder:text-black"
-  />
-</section>
+{#if data.data?.alert}
+  <div class="bg-alert mb-3 p-1 text-center">{data.data?.alert}</div>
+{/if}
+{#if error}
+  <div class="mb-3 break-all text-red-400">{error}</div>
+{/if}
+
+{#if data.data?.note}
+  <section id="content">
+    <textarea
+      rows="13"
+      name="note"
+      value={data.data?.note.note}
+      readonly
+      placeholder="Write your note here..."
+      class="w-full !bg-yellow-100 !bg-opacity-75 p-5 text-black outline-none placeholder:text-black"
+    />
+  </section>
+{/if}
