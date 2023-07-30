94e415ea56573aab77fab3788d03709e119fda5b
Don-Cryptus <don.cryptus@gmail.com>
2023-07-30 17:01:54 +0200

    easy

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -1,19 +1,25 @@
 <script>
   import 'uno.css';
   import '../app.css';
-  import Header from '$lib/components/container/Header.svelte';
   import Footer from '$lib/components/container/Footer.svelte';
+  import Discord from '$lib/svg/Discord.svelte';
+  import Github from '$lib/svg/Github.svelte';
 </script>
 
-<header class="bg-main py-3">
-  <section class="container flex items-center space-x-5 px-2">
-    <a href="/"><img alt="logo" src="/privnote-logo.svg" class="cursor-cell" /></a>
-    <p class="w-64 text-sm">Send notes will self-destruct after being read.</p>
-  </section>
+<header class="bg-main">
+  <div class="container py-3 items-center flex justify-between">
+    <section class="container flex items-center space-x-5 px-2">
+      <a href="/"><img alt="logo" src="/privnote-logo.svg" class="cursor-cell" /></a>
+      <p class="w-64 text-sm">Send notes will self-destruct after being read.</p>
+    </section>
+    <section class="flex space-x-2">
+      <a href="https://discord.gg/coding" target="_blank"><Discord /></a>
+      <a href="https://github.com/Don-Cryptus/privnote" target="_blank"><Github /></a>
+    </section>
+  </div>
 </header>
 <hr class="border-alt border-8" />
 <section class="container flex flex-col justify-between">
- 
   <slot />
 </section>
 <Footer />
