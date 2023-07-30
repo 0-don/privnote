63da5d27f58c0aae9ee05be93ffa414973ebfa8b
Don-Cryptus <don.cryptus@gmail.com>
2023-07-30 15:00:14 +0200

    test

--- web/src/lib/utils/client/constants.ts
+++ web/src/lib/utils/client/constants.ts
@@ -2,7 +2,7 @@ import type { ResponseBody, Messages } from '$lib/@types';
 
 export const debugLog = (data: ResponseBody, form: ResponseBody) => ({
   data: [
-    ...(data?.data ? [{ key: 'response', path: 'data', message: data?.data }] : []),
+    ...(data?.data ? [{ key: 'response', path: 'data', message: data?.data }] as any : []),
     ...(form?.data ? [{ key: 'request', path: 'form', message: form?.data }] : [])
   ] as Messages[],
   form: (form?.messages || []).map((m) => ({ ...m, key: 'form' })),
