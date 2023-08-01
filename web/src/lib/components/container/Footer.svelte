40413ee435b38451d01812b441c6af27aa50b3e8
Don-Cryptus <don.cryptus@gmail.com>
2023-08-02 00:29:13 +0200

    test

--- web/src/lib/components/container/Footer.svelte
+++ web/src/lib/components/container/Footer.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 </script>
 
-<footer class="sticky top-[100dvh] bg-main py-0.5 mt-5">
+<footer class="sticky top-[100dvh] bg-main py-0.5 mt-5 max-h-full">
   <div class="container flex justify-between">
     <a type="a" href="/" aria-label="Write a new note" class="underline hover:no-underline">Write a new note</a>
     <div>
