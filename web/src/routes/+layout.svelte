c569299b2f6eb670fea849f26dc237213c735f4d
Don-Cryptus <don.cryptus@gmail.com>
2023-08-06 22:48:44 +0200

    easy

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -10,7 +10,9 @@
 <header class="bg-main">
   <div class="container py-3 items-center flex justify-between h-full">
     <section class="container flex items-center space-x-5 px-2 w-full">
-      <a aria-label="Go To Homepage" href="/"><PrivnoteLogo /></a>
+      <a aria-label="Go To Homepage" href="/"
+        ><img alt="logo" width="200" height="50" src="/privnote-logo.svg" class="cursor-cell" /></a
+      >
       <p class="w-32 md:w-60 text-xs md:text-sm">Sent notes will self-destruct after being read.</p>
     </section>
     <section class="flex flex-col justify-between items-center h-full space-y-4 md:flex-row md:space-y-0 md:space-x-4">
